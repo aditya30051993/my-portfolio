7a071d07882b4e143018e185a9f7af216a659af0
Aditya <aditya30051993@gmail.com>
2024-09-21 17:51:30 +0530

    added about

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const About = () => (
   <section className="about" id="about">
     <h2>👋 Hello!</h2>
     <p>
-      I'm a dedicated medical officer passionate about AI and entrepreneurship. My journey in healthcare has sparked a desire to explore innovative solutions that bridge medicine and technology. With an <a href="#education">MBBS degree</a>, I have a strong foundation in clinical practices, driving me to enhance healthcare through <a href="#skills">advanced technologies</a>.
+      I'm a dedicated medical officer passionate about AI and entrepreneurship.
+      My journey in healthcare has sparked a desire to explore innovative
+      solutions that bridge medicine and technology.
     </p>
     <br />
     <p>
-      I believe AI can transform patient experiences and outcomes. By leveraging data-driven insights, I aim to <a href="#projects">create advancements</a> that improve diagnostic accuracy and treatment efficiency. 🚀
+      With an <a href="#education">MBBS</a> degree, I have a strong foundation
+      in clinical practices, driving me to enhance healthcare through advanced{" "}
+      <a href="#skills"> technologies</a>.
+    </p>
+    <br />
+    <p>
+      I believe AI can transform patient experiences and outcomes. By leveraging
+      data-driven insights, I aim to create <a href="#projects">advancements</a>{" "}
+      that improve diagnostic accuracy and treatment efficiency. 🚀
     </p>
   </section>
 );
