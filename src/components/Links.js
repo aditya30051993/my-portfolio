583a7d31ba24c508be8f49e9cf030be60e58873d
Aditya <aditya30051993@gmail.com>
2024-09-21 13:05:54 +0530

    added card grids

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,25 +5,25 @@ const Links = () => {
   const linkData = [
     {
       url: 'https://www.upwork.com/freelancers/~01c2bfda01cedbb871',
-      title: 'Upwork Profile',
+      title: 'Upwork',
       description: 'Find my professional profile on Upwork, where I offer my services.',
       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Upwork-logo.svg/640px-Upwork-logo.svg.png',
     },
     {
       url: 'https://medium.com/@adi93',
-      title: 'Medium Articles',
-      description: 'Read my articles on Medium where I write about health, tech, and more.',
+      title: 'Medium',
+      description: 'Read my articles on Medium where I write about tech in health and more.',
       image: 'https://miro.medium.com/v2/resize:fit:1400/1*psYl0y9DUzZWtHzFJLIvTw.png',
     },
     {
       url: 'https://www.linkedin.com/in/adi0593/',
-      title: 'LinkedIn Profile',
-      description: 'Connect with me on LinkedIn for professional networking and career opportunities.',
+      title: 'LinkedIn',
+      description: 'Connect with me on LinkedIn for professional networking.',
       image: 'https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg',
     },
     {
       url: 'https://github.com/aditya30051993',
-      title: 'GitHub Profile',
+      title: 'GitHub',
       description: 'Explore my repositories and contributions on GitHub.',
       image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
     }
